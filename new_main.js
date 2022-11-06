34c7f3e8b00ff3ca2b2bdfd4af060bfac33fd909
Raf <rawiczhub@gmail.com>
2022-11-06 22:11:58 +0000

    videos sorted with separate functions

--- new_main.js
+++ new_main.js
@@ -6,7 +6,7 @@ function mainClick(){
     
     const mainSection = document.getElementById('mouseClick');
     mainSection.addEventListener('click', showDiv)
-    console.log(mainSection)
+    // console.log(mainSection)
 function showDiv(e){
     let x = e.clientX
     let y = e.clientY
@@ -30,26 +30,125 @@ function showDiv(e){
     
 };
 }
-function playPause() { 
+function playPause1() { 
 
-
-    
-const myVideo = document.getElementById('video')
-
-
-    
-    if (myVideo.paused) {
-        // myVideo.setAttribute('style', 'width: 50vh')
+const myVideo = document.getElementById('video1');
+if (myVideo.paused) {                         
+                    myVideo.classList.toggle('expanded')
+                    myVideo.play();                        
+                    }
+else {
         myVideo.classList.toggle('expanded')
-        myVideo.play();
+        myVideo.pause(); }
+}
+    
+function playPause2() { 
 
-    }
-    //   myVideo.play(); 
+    const myVideo = document.getElementById('video2');
+    if (myVideo.paused) {                         
+                        myVideo.classList.toggle('expanded')
+                        myVideo.play();                        
+                        }
     else {
-        // myVideo.setAttribute('style', 'width: 20vh' );
-        myVideo.classList.toggle('expanded')
-        myVideo.pause(); 
+            myVideo.classList.toggle('expanded')
+            myVideo.pause(); }
     }
+    function playPause3() { 
+
+        const myVideo = document.getElementById('video3');
+        if (myVideo.paused) {                         
+                            myVideo.classList.toggle('expanded')
+                            myVideo.play();                        
+                            }
+        else {
+                myVideo.classList.toggle('expanded')
+                myVideo.pause(); }
+        }
+        function playPause4() { 
+
+            const myVideo = document.getElementById('video4');
+            if (myVideo.paused) {                         
+                                myVideo.classList.toggle('expanded')
+                                myVideo.play();                        
+                                }
+            else {
+                    myVideo.classList.toggle('expanded')
+                    myVideo.pause(); }
+            }
+
+
+
+
+// const myVideos = document.getElementsByTagName('video');
+// console.log(myVideos)
+// for (let i = 0; i < myVideos.length; i++) {
+//     if (myVideos[i].paused) {                         
+//                         myVideos[i].classList.toggle('expanded')
+//                         myVideos[i].play();                        
+//                     }
+//                     else {
+//                         myVideos[i].classList.toggle('expanded')
+//                         myVideos[i].pause(); 
+                
+//                     }}
+
+
+
+// for (let i = 0; i < myVideos.length; i++) {
+//     (function(index) {
+//          myVideos[index].addEventListener("click", function() {
+//             // console.log("you clicked region number " + index);
+//             if (myVideos[i].paused) {
+//                 // myVideo.setAttribute('style', 'width: 50vh')
+//                 myVideos[i].classList.toggle('expanded')
+//                 myVideos[i].play();
+                
+//             }
+//             //   myVideo.play(); 
+//             else {
+//                 // myVideo.setAttribute('style', 'width: 20vh' );
+//                 myVideos[i].classList.toggle('expanded')
+//                 myVideos[i].pause(); 
+//             }
+//           })
+//     })(i);
+//  }
+
+
+// for (let i = 0; i < myVideos.length; i++) {
+    
+//          myVideos[i].addEventListener("click", function() {
+//             console.log("you clicked region number " + i);
+//             if (myVideos[i].paused) {
+//                 // myVideo.setAttribute('style', 'width: 50vh')
+//                 myVideos[i].classList.toggle('expanded')
+//                 myVideos[i].play();
+                
+//             }
+//             //   myVideo.play(); 
+//             else {
+//                 // myVideo.setAttribute('style', 'width: 20vh' );
+//                 myVideos[i].classList.toggle('expanded')
+//                 myVideos[i].pause(); 
+//             }
+//           })
+    
+//  }
+
+
+
+    
+    // if (myVideos[1].paused) {
+    //     // myVideo.setAttribute('style', 'width: 50vh')
+    //     myVideos[1].classList.toggle('expanded')
+    //     myVideos[1].play();
+
+    // }
+    // //   myVideo.play(); 
+    // else {
+    //     // myVideo.setAttribute('style', 'width: 20vh' );
+    //     myVideos[1].classList.toggle('expanded')
+    //     myVideos[1].pause(); 
+    // }
       
-  } 
 
